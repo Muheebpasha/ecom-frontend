adb0c1e77214830248ecdaf1eff39453bd7bebc4
Muheeb Pasha <muheebpasha0@gmail.com>
2025-10-12 21:14:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { FaBeer } from 'react-icons/fa'
+import Products from './components/Products'
+
+
+function App() {
+  
+
+  return (
+    <>
+       <Products />
+    </>
+  )
+}
+
+export default App
